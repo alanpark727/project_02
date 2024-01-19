6d9417e9efce270fa207e1ccce1df2e1ee24a8b4
alanpark727 <drasel1234@naver.com>
2024-01-19 17:38:37 +0900

    2024.01.19_project02
    반응형_웹사이트/project_02
    common 파트 헤더, 푸터 1차 완성(반응형 적용까지)
    +사용할 이미지들과 텍스트 준비하기
    ++슬라이더에 슬라이드형식 이미지 바꾸기 기능 적용하기

---
+++ js/common.js
@@ -0,0 +1,48 @@
+// Common Part (Header, Footer)
+// j-Query
+
+$(document).ready(function () {
+    $("#hamburger").click(function () {
+        $(this).toggleClass("is-active");
+    });
+});
+$(function () {
+    $("#hamburger").click(function () {
+        if ($('#hamburger').hasClass('is-active')) {
+            $('#header_menu').slideToggle();
+        } else {
+            $('#header_menu').hide();
+        }
+    });
+});
+$(window).resize(function () {
+    if (window.innerWidth >= 785) {
+        $('#header_menu').show();
+    } else $('#header_menu').hide();
+
+});
+$(function (){
+    $('#h_nav').click(function () {
+        $('#h_sub').slideToggle();
+    });
+});
+$(function (){
+    $('#n_nav').click(function () {
+        $('#n_sub').slideToggle();
+    });
+});
+$(function (){
+    $('#a_nav').click(function () {
+        $('#a_sub').slideToggle();
+    });
+});
+$(function (){
+    $('#e_nav').click(function () {
+        $('#e_sub').slideToggle();
+    });
+});
+$(function (){
+    $('#g_nav').click(function () {
+        $('#g_sub').slideToggle();
+    });
+});
